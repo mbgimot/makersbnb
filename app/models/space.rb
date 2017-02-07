4e4cbaa3088fff4d5ead7e6101835e6ba05c06af
micawhitby <micawhitby@gmail.com>
2017-02-07 14:21:56 +0000

    added date available property

--- app/models/space.rb
+++ app/models/space.rb
@@ -7,6 +7,7 @@ class Space
   property :description, Text, :length => 500, :required => true
   property :price, Decimal, precision: 15, scale: 2, :required => true
   property :availability, Boolean, :default => true
+  property :date_available, DateTime
   #belongs_to :user
 
 end
