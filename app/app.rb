02330a84217070dcaf34b8b13fbb6e92a1e2720e
Mitchell Goldbay <mitchellgoldbay@gmail.com>
2017-02-07 11:49:48 +0000

    user can sign up

--- app/app.rb
+++ app/app.rb
@@ -1,22 +1,47 @@
 ENV['RACK_ENV'] ||= 'development'
 
 require 'sinatra/base'
-require 'sinatra/flash'
-
 require_relative 'datamapper_setup'
+require 'sinatra/flash'
+require 'encrypted_cookie'
+require_relative 'helpers'
 
 
 class MakersBnB < Sinatra::Base
-
+  set :public_folder, Proc.new { File.join(root, 'static') }
   register Sinatra::Flash
   use Rack::MethodOverride
+  use Rack::Session::EncryptedCookie,
+    secret: '1ad3e5c2b617e329aad83a5749d133ea426070d31bd2e11f9f4df626f966a259'
+
 
   DataMapper::Logger.new($stdout, :debug)
 
+  helpers Helpers
 
   get '/' do
     'Hello MakersBnB!'
   end
+
+  get '/users/new' do
+    session[:email] ? @email_res = session[:email] : nil
+    session[:name] ? @name_res = session[:name] : nil
+    erb(:'users/new')
+  end
+
+  post '/users/new' do
+    user = User.create(name: params[:name], email: params[:email], password: params[:password], password_conf: params[:password_conf])
+    if user.save
+      session[:user_id] = user.id
+      redirect '/spaces'
+    else
+      session[:email] = params[:email]
+      session[:name] = params[:name]
+      flash[:errors] = user.errors.full_messages
+      redirect '/users/new'
+    end
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
